059a9f98691910c724d908091ce58d429a9c9e9b
James Cox <jacox@jacox.net>
2022-12-05 19:00:51 -0600

    add bio and pic

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     // Use buttons to toggle between views
+    document.querySelector('#bio').addEventListener('click', () => gotobio());
     document.querySelector('#imovie').addEventListener('click', () => gotoimovie());
     document.querySelector('#garageband').addEventListener('click', () => gotogarageband());
     document.querySelector('#powerpoint').addEventListener('click', () => gotopowerpoint());
     document.querySelector('#scratch').addEventListener('click', () => gotoscratch());
 
-    gotoimovie();
+    gotobio();
 });
 
 function blankall() {
     // disable all display divs
+    document.querySelector('#aboutmediv').style.display = 'none';
     document.querySelector('#imoviediv').style.display = 'none';
     document.querySelector('#garagebanddiv').style.display = 'none';
     document.querySelector('#powerpointdiv').style.display = 'none';
     document.querySelector('#scratchdiv').style.display = 'none';
     
     // make all buttons inactive
+    document.querySelector('#bio').classList.remove('active');
     document.querySelector('#imovie').classList.remove('active');
     document.querySelector('#garageband').classList.remove('active');
     document.querySelector('#powerpoint').classList.remove('active');
     document.querySelector('#scratch').classList.remove('active');
 
+}
+function gotobio() {
+    blankall();
+    document.querySelector('#aboutmediv').style.display = 'block';
+    document.querySelector('#bio').classList.add('active');
 }
 
 function gotoimovie() {
